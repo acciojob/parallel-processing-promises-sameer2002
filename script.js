051299c95ed623b02f91019c1da7abecc43a1173
Sameer Sagar <60567209+sameer2002@users.noreply.github.com>
2023-09-02 16:57:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+function renderImages(arr){
+	//
+	Promise.all(arr)
+	.then((data)=>{
+	console.log(data);
+	data.forEach(element => {
+		let img=document.createElement("img");
+		img.src=element.url;
+		output.appendChild(img);
+	});
+})
+}
+
+btn.addEventListener("click",()=>{
+	return renderImages(images);
+});
